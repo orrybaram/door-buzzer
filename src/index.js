5f2681cd86ea2eec5b4c38a9e4ce8af98636de0d
Orry Baram <orrybaram@gmail.com>
2017-11-02 22:19:47 -0400

    injects globals

--- src/index.js
+++ src/index.js
@@ -1,8 +1,19 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
+import { injectGlobal } from 'styled-components'
 import App from './App';
 import registerServiceWorker from './registerServiceWorker';
+
+injectGlobal`
+  html, body, #root {
+    height: 100%;
+  }
+  body {
+    margin: 0;
+    padding: 0;
+    font-family: sans-serif;
+  }
+`;
 
 ReactDOM.render(<App />, document.getElementById('root'));
 registerServiceWorker();
