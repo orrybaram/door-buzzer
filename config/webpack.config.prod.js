766745602a4513eed2c3ae7b23dc1718386e37ca
Orry Baram <orrybaram@gmail.com>
2017-11-02 23:46:56 -0400

    removes css modules

--- config/webpack.config.prod.js
+++ config/webpack.config.prod.js
@@ -183,8 +183,6 @@ module.exports = {
                         importLoaders: 1,
                         minimize: true,
                         sourceMap: shouldUseSourceMap,
-                        modules: true,
-                        localIdentName: "[name]__[local]___[hash:base64:5]"
                       },
                     },
                     {
